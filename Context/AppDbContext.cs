45c0b563531e70611213640083baa7a7a41d724d
Rian Porfírio <rianporfiriodev@gmail.com>
2024-10-12 17:38:29 -0300

    first commit

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,46 @@
+﻿using EShop.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.ProductAPI.Context;
+
+public class AppDbContext : DbContext
+{
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    //Fluent API
+
+    protected override void OnModelCreating(ModelBuilder md)
+    {
+        md.Entity<Category>().HasKey(c => c.Id);
+
+        md.Entity<Category>().Property(c => c.Name).HasMaxLength(255).IsRequired();
+
+        md.Entity<Category>().HasMany(c => c.Products).WithOne(p => p.Category).OnDelete(DeleteBehavior.Cascade);
+
+
+        md.Entity<Product>().Property(p => p.Name).HasMaxLength(255).IsRequired();
+
+        md.Entity<Product>().Property(p => p.Description).HasMaxLength(255).IsRequired();
+
+        md.Entity<Product>().Property(p => p.ImageUrl).HasMaxLength(255).IsRequired();
+
+        md.Entity<Product>().Property(p => p.Price).HasPrecision(6,3).IsRequired();
+
+        md.Entity<Category>().HasData(
+            new Category
+            {
+                Id = 1,
+                Name = "Material Escolar",
+            },
+            new Category
+            {
+                Id = 2,
+                Name = "Acessórios",
+            }
+        );
+    }
+}
---
+++ Migrations/20241011004423_SeedProducts.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EShop.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("""
+                INSERT INTO products(Name, Price, Description, ImageUrl, CategoryId)
+                VALUES("Borracha", 2.90, "Feita para apagar", "photo.png", 2)
+                """);
+            mb.Sql("""
+                INSERT INTO products(Name, Price, Description, ImageUrl, CategoryId)
+                VALUES("Lápis", 3.90, "Feito para escrever", "photo.png", 2)
+                """);
+            mb.Sql("""
+                INSERT INTO products(Name, Price, Description, ImageUrl, CategoryId)
+                VALUES("Caderno", 20.90, "Feito para ser escrito e apagado", "photo.png", 2)
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DROP FROM products");
+        }
+    }
+}
